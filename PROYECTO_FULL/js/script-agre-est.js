23a335837411e62aaa5bda9483978788feed1e5f
AdrianaMoralesC4 <adrianamoralesc19@gmail.com>
2024-08-29 11:49:30 -0500

    proyecto

---
+++ PROYECTO_FULL/js/script-agre-est.js
@@ -0,0 +1,31 @@
+function agregar_est(){
+    var table = document.getElementById('table')
+    var nombre = document.getElementById("nombre")
+    var apellido = document.getElementById("apellido")
+    var curso = document.getElementById("curso")
+
+    var tr = document.createElement('tr')
+
+    var td1 = tr.appendChild(document.createElement('td'))
+    var td2 = tr.appendChild(document.createElement('td'))
+    var td3 = tr.appendChild(document.createElement('td'))
+    var td4 = tr.appendChild(document.createElement('td'))
+
+    var nombreTexto = document.createTextNode(nombre.value)
+    var apellidoTexto = document.createTextNode(apellido.value)
+    var cursoTexto = document.createTextNode(curso.value)
+    var puntoTexto = document.createTextNode('0')
+
+
+    td1.appendChild(nombreTexto)
+    td2.appendChild(apellidoTexto)
+    td3.appendChild(cursoTexto)
+    td4.appendChild(puntoTexto)
+
+    table.appendChild(tr)
+
+    //LIMPIA LOS CAMPOS DE ENTRADA
+    nombre.value = ''
+    apellido.value = ''
+    curso.value = ''
+}
---
+++ test_conex_db_2/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bodyparser = require('body-parser')
+const path = require('path')
+
+const app = express()
+
+dotenv.config({path: 'config.env'})
+const PORT = process.env.PORT || 8080
+
+//log requests
+app.use(morgan('tiny'))
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended: true}))
+
+
+//set view engine
+app.set('view engine','ejs')
+//app.set('views', path.resolve(__dirname, 'views/ejs'))
+
+//load assests
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')))
+// si se desea acceder a los estilos de css en ves de '/css' se pone '/css/style.css'
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')))
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')))
+
+
+
+app.get('/',(req, res) =>{
+    res.render('index')
+})
+
+app.listen(PORT, ()=> {console.log(`Server is running on http://localhost:${PORT}`)})
